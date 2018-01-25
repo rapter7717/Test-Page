28991b7f9c7701babbeb80cde3816254c4af3cea
rapter7717 <35534590+rapter7717@users.noreply.github.com>
2018-01-24 20:25:23 -0600

    Assignment 1

---
+++ Assignment/01-assignment.php
@@ -0,0 +1,65 @@
+<?php
+
+/*****************************************
+*************  Instructions  *************
+******************************************
+         Fill in your responses
+        to this assignment below.
+*****************************************/
+
+
+
+// 01. Create and use a string variable
+$test = "This is a test" ;
+
+
+
+
+
+
+
+// 02. Create and use an integer variable
+$one = 1 ;
+$two = 2 ;
+
+
+
+
+
+
+// 03. Create and use a boolean variable
+
+
+
+
+
+// 04. Create and call one or more function(s)
+
+
+
+
+
+// 05. Basic conditional statement
+
+
+
+
+
+// 01. Echo to page
+
+
+
+
+
+?>
+
+<html>
+
+<head> 
+	<title> <?php echo $test; ?> </title>
+</head>
+</br> 
+
+<body> <p> <?php echo $one + $two ; ?> </p>
+
+	</body>
---
+++ Test Page.php
@@ -0,0 +1,14 @@
+<?php
+$test = "Hello World!"
+
+
+?>
+
+<html>
+
+<head>
+ <h1> <?php echo $test ; ?> </h1>
+
+</head>
+<img src= "img/file structure.jpeg" >
+</html>
